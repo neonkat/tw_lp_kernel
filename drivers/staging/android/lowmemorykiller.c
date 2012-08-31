9d6e7f7d89bfc4f57e0d3b50130d9100713f7087
Jeffrey Carlyle <jeff.carlyle@motorola.com>
2012-08-31 03:55:52 -0500

    trace: memkill: trace LMK and OOM kills
    How to enable tracer:
    
      # mount -t debugfs nodev /sys/kernel/debug
      # echo 1 > /sys/kernel/debug/tracing/tracing_on
      # echo 1 > /sys/kernel/debug/tracing/events/memkill/enable
      # cat /sys/kernel/debug/tracing/trace
    
    Event format:
    
      oom_kill: <pid> <comm> <total_vm> <anon_rss> <file_rss> <oom_score_adj> <order> <points> <cached> <freeswap>
      oom_kill_shared: <pid> <comm>
      lmk_kill: <pid> <comm> <selected_oom_adj> <selected_tasksize> <min_adj> <cached> <freeswap>
    
    Change-Id: Ic45e181c62aead9b19d2a40363217f9220896096
    Signed-off-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/480746
    Reviewed-on: http://gerrit.pcs.mot.com/513174
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Thirumarai Selvi S <a16462@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit ef733ddfbb73cd33daf36720fa474ce4c8acb1f7)
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -61,6 +61,12 @@ static uint32_t oom_count = 0;
 
 #ifdef MULTIPLE_OOM_KILLER
 #define OOM_DEPTH 5
+#include <trace/events/memkill.h>
+
+#ifdef CONFIG_HIGHMEM
+#define _ZONE ZONE_HIGHMEM
+#else
+#define _ZONE ZONE_NORMAL
 #endif
 
 static uint32_t lowmem_debug_level = 1;
@@ -324,6 +330,9 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 				nr_cma_free, sc->priority);
 #endif
 		lowmem_deathpending_timeout = jiffies + HZ;
+		trace_lmk_kill(selected->pid, selected->comm,
+				selected_oom_score_adj, selected_tasksize,
+				min_score_adj);
 		send_sig(SIGKILL, selected, 0);
 		set_tsk_thread_flag(selected, TIF_MEMDIE);
 		rem -= selected_tasksize;
