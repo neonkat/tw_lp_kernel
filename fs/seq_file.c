45fa9f805fd5184726baf44bb5f5ac5c9a10a3ea
Jordan Crouse <jcrouse@codeaurora.org>
2014-09-10 10:09:53 -0600

    fs/seq_file: Use vmalloc by default for allocations > PAGE_SIZE
    Some OOM implementations are pretty trigger happy when it comes to
    releasing memory for kmalloc() allocations.  We might as well head
    straight to vmalloc for allocations over PAGE_SIZE.
    
    Bug: 17871993
    Change-Id: Ic0dedbadc8bf551d34cc5d77c8073938d4adef80
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>
    
    Conflicts:
    	fs/seq_file.c

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -33,6 +33,18 @@ static void seq_set_overflow(struct seq_file *m)
 	m->count = m->size;
 }
 
+static void *seq_buf_alloc(unsigned long size)
+{
+	void *buf;
+
+	if (size > PAGE_SIZE)
+		buf = vmalloc(size);
+	else
+		buf = kmalloc(size, GFP_KERNEL | __GFP_NOWARN);
+
+	return buf;
+}
+
 /**
  *	seq_open -	initialize sequential file
  *	@file: file we initialize
