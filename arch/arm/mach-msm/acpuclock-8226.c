d4fdc4056de0754b9af3cd368c4c8dd0a37f96dc
neonkat <neonkatxda@gmail.com>
2016-05-13 18:07:26 +0530

    attempt to fix uc
    Conflicts:
    	arch/arm/configs/ms013g_defconfig

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -62,8 +62,10 @@ static struct msm_bus_scale_pdata bus_client_pdata = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
+#ifdef CPU_UNDERCLOCK
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+#endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -75,8 +77,10 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p1[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
+#ifdef CPU_UNDERCLOCK
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+#endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -88,8 +92,10 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p2[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
+#ifdef CPU_UNDERCLOCK
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+#endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -104,8 +110,10 @@ static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p4[] = {
 };
 
 static struct clkctl_acpu_speed acpu_freq_tbl_8226_1p6[] = {
+#ifdef CPU_UNDERCLOCK
 	{ 1,  192000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  249600, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
+#endif
 	{ 1,  300000, PLL0,    4, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  384000, ACPUPLL, 5, 2,   CPR_CORNER_2,    0, 4 },
 	{ 1,  600000, PLL0,    4, 0,   CPR_CORNER_4, 0, 6 },
@@ -158,8 +166,13 @@ static struct acpuclk_drv_data drv_data = {
 		.update_mask = RCG_CONFIG_UPDATE_BIT,
 		.poll_mask = RCG_CONFIG_UPDATE_BIT,
 	},
+#ifdef CONFIG_CPU_UNDERCLOCK
 	.power_collapse_khz = 192000,
 	.wait_for_irq_khz = 192000,
+#else
+	.power_collapse_khz = 192000,
+	.wait_for_irq_khz = 192000,
+#endif
 };
 
 static int __init acpuclk_a7_probe(struct platform_device *pdev)
